b7da0098fa10031c76ae13fe438eacb397751d19
Sylvance <kerandisylvance@gmail.com>
2018-08-27 03:01:59 +0300

    Add goods model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180826235457) do
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "username", null: false
@@ -20,4 +19,11 @@ ActiveRecord::Schema.define(version: 20180826235457) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "goods", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goods_on_user_id"
+  end
 end
